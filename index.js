6def97904d8c1c2afcc7618f545c059e72e41831
MohammadSoleimanikia <121093560+MohammadSoleimanikia@users.noreply.github.com>
2024-11-25 20:41:32 +0330

    add select all and insert into todo table with postgresql

--- index.js
+++ index.js
@@ -1,27 +1,34 @@
 import express from "express";
 import bodyParser from "body-parser";
+import pg from "pg";
 
 const app = express();
 const port = 3000;
 
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "world",
+  password: "12345",
+  port: 5432,
+});
+db.connect();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-let items = [
-  { id: 1, title: "Buy milk" },
-  { id: 2, title: "Finish homework" },
-];
 
-app.get("/", (req, res) => {
+// use async to wait for response
+app.get("/",async (req, res) => {
+  const result =await db.query("SELECT * FROM todolist;");
   res.render("index.ejs", {
     listTitle: "Today",
-    listItems: items,
+    listItems: result.rows,
   });
 });
 
-app.post("/add", (req, res) => {
-  const item = req.body.newItem;
-  items.push({ title: item });
+app.post("/add",async (req, res) => {
+  
+  const result =await db.query("INSERT INTO todolist (title) VALUES ($1);",[req.body.newItem]);
   res.redirect("/");
 });
 
