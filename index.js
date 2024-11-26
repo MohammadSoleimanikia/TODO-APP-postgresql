82064e6a0ea319f9c4df5765984b2050623a8711
MohammadSoleimanikia <121093560+MohammadSoleimanikia@users.noreply.github.com>
2024-11-26 21:06:57 +0330

    add try catch block to handle errors

--- index.js
+++ index.js
@@ -16,29 +16,47 @@ db.connect();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-
 // use async to wait for response
-app.get("/",async (req, res) => {
-  const result =await db.query("SELECT * FROM todolist ORDER BY id ASC;");
+app.get("/", async (req, res) => {
+  const result = await db.query("SELECT * FROM todolist ORDER BY id ASC;");
   res.render("index.ejs", {
     listTitle: "Today",
     listItems: result.rows,
   });
 });
 
-app.post("/add",async (req, res) => {
-  const result =await db.query("INSERT INTO todolist (title) VALUES ($1);",[req.body.newItem]);
-  res.redirect("/");
+app.post("/add", async (req, res) => {
+  try {
+    const result = await db.query("INSERT INTO todolist (title) VALUES ($1);", [
+      req.body.newItem,
+    ]);
+    res.redirect("/");
+  } catch (err) {
+    console.log(err);
+  }
 });
 
-app.post("/edit",async (req, res) => {
-  const result =await db.query("UPDATE todolist SET title =$1 WHERE id=$2;",[req.body.updatedItemTitle,req.body.updatedItemId]);
-  res.redirect("/");
+app.post("/edit", async (req, res) => {
+  try {
+    const result = await db.query(
+      "UPDATE todolist SET title =$1 WHERE id=$2;",
+      [req.body.updatedItemTitle, req.body.updatedItemId]
+    );
+    res.redirect("/");
+  } catch (err) {
+    console.log(err);
+  }
 });
 
-app.post("/delete", async(req, res) => {
-  const result =await db.query("Delete from todolist Where id=$1;",[req.body.deleteItemId]);
-  res.redirect("/");
+app.post("/delete", async (req, res) => {
+  try {
+    const result = await db.query("Delete from todolist Where id=$1;", [
+      req.body.deleteItemId,
+    ]);
+    res.redirect("/");
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 app.listen(port, () => {
